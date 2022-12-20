2a3e2bbe42b3f2270122e80981e1b7eda0e11055
Alex Gorodnitskiy <alekc1272@gmail.com>
2022-12-20 20:34:55 +0300

    setup project

---
+++ setup.py
@@ -0,0 +1,15 @@
+from setuptools import find_packages, setup
+
+
+setup(
+    name="src",
+    version="0.1.0",
+    description=(
+        "Deep Learning training pipeline template"
+        "based on pytorch_lightning and hydra"
+    ),
+    author="Your Name",
+    author_email="Your Email",
+    install_requires=["pytorch-lightning", "hydra-core"],
+    packages=find_packages(exclude=['tests']),
+)
