4dd80a8d0791189c7de289270e6a168fd51900b8
Alex Gorodnitskiy <alekc1272@gmail.com>
2022-12-21 12:09:55 +0300

    add sweeper tests

---
+++ tests/test_sweeps.py
@@ -0,0 +1,88 @@
+import pytest
+
+from tests.helpers.run_if import RunIf
+from tests.helpers.run_sh_command import run_sh_command
+
+startfile = "src/train.py"
+overrides = ["logger=[]"]
+
+
+@RunIf(sh=True)
+@pytest.mark.slow
+def test_experiments(tmp_path):
+    """Test running all available experiment configs with fast_dev_run=True."""
+    command = [
+        startfile,
+        "-m",
+        "experiment=glob(*)",
+        "hydra.sweep.dir=" + str(tmp_path),
+        "++trainer.fast_dev_run=true",
+    ] + overrides
+    run_sh_command(command)
+
+
+@RunIf(sh=True)
+@pytest.mark.slow
+def test_hydra_sweep(tmp_path):
+    """Test default hydra sweep."""
+    command = [
+        startfile,
+        "-m",
+        "hydra.sweep.dir=" + str(tmp_path),
+        "module.optimizer.lr=0.005,0.01",
+        "++trainer.fast_dev_run=true",
+    ] + overrides
+    run_sh_command(command)
+
+
+@RunIf(sh=True)
+@pytest.mark.slow
+def test_hydra_sweep_ddp_sim(tmp_path):
+    """Test default hydra sweep with ddp sim."""
+    command = [
+        startfile,
+        "-m",
+        "hydra.sweep.dir=" + str(tmp_path),
+        "trainer=ddp_sim",
+        "trainer.max_epochs=3",
+        "+trainer.limit_train_batches=0.01",
+        "+trainer.limit_val_batches=0.1",
+        "+trainer.limit_test_batches=0.1",
+        "module.optimizer.lr=0.005,0.01,0.02",
+    ] + overrides
+    run_sh_command(command)
+
+
+@RunIf(sh=True)
+@pytest.mark.slow
+def test_optuna_sweep(tmp_path):
+    """Test optuna sweep."""
+    command = [
+        startfile,
+        "-m",
+        "hparams_search=mnist_optuna",
+        "hydra.sweep.dir=" + str(tmp_path),
+        "hydra.sweeper.n_trials=10",
+        "hydra.sweeper.sampler.n_startup_trials=5",
+        "++trainer.fast_dev_run=true",
+    ] + overrides
+    run_sh_command(command)
+
+
+@RunIf(sh=True)
+@pytest.mark.slow
+def test_optuna_sweep_ddp_sim(tmp_path):
+    """Test optuna sweep with ddp sim."""
+    command = [
+        startfile,
+        "-m",
+        "hparams_search=mnist_optuna",
+        "hydra.sweep.dir=" + str(tmp_path),
+        "hydra.sweeper.n_trials=5",
+        "trainer=ddp_sim",
+        "trainer.max_epochs=3",
+        "+trainer.limit_train_batches=0.01",
+        "+trainer.limit_val_batches=0.1",
+        "+trainer.limit_test_batches=0.1",
+    ]
+    run_sh_command(command)
