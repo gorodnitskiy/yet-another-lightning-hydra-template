da67e0568b723e18444a87ea5439577ad35ec029
Alex Gorodnitskiy <alekc1272@gmail.com>
2022-12-21 12:06:43 +0300

    add modules tests

---
+++ tests/test_load_losses.py
@@ -0,0 +1,34 @@
+import pytest
+import omegaconf
+
+from src.modules.losses.losses import load_loss
+
+
+_IMPLEMENTED_LOSS_NAMES = (
+    "AngularPenaltySMLoss",
+    "FocalLossManual",
+    "segmentation_models_pytorch/DiceLoss",
+    "segmentation_models_pytorch/FocalLoss",
+    "segmentation_models_pytorch/JaccardLoss",
+    "segmentation_models_pytorch/LovaszLoss",
+    "segmentation_models_pytorch/TverskyLoss",
+    "torch.nn/BCEWithLogitsLoss",
+    "torch.nn/BCELoss",
+    "torch.nn/CrossEntropyLoss",
+    "torch.nn/L1Loss",
+    "torch.nn/MSELoss",
+    "torch.nn/NLLLoss",
+    "torch.nn/SmoothL1Loss",
+    "VicRegLoss",
+)
+
+
+@pytest.mark.parametrize("loss_name", _IMPLEMENTED_LOSS_NAMES)
+def test_loss(loss_name: str):
+    cfg = {"weighted": False, "name": loss_name, "params": None}
+    if "segmentation_models_pytorch/" in loss_name:
+        cfg.update({"params": {"mode": "binary"}})
+    elif loss_name == "AngularPenaltySMLoss":
+        cfg.update({"params": {"embedding_size": 16, "num_classes": 10}})
+    cfg = omegaconf.OmegaConf.create(cfg)
+    _ = load_loss(cfg)
---
+++ tests/test_load_metrics.py
@@ -0,0 +1,60 @@
+from typing import Any, Dict, Tuple
+
+import pytest
+import omegaconf
+
+from src.modules.metrics.metrics import load_metric, load_metrics
+
+_IMPLEMENTED_METRICS = (
+    ("torchmetrics/Accuracy", {"task": "binary"}),
+    ("torchmetrics/AUROC", {"task": "binary"}),
+    ("torchmetrics/FBetaScore", {"task": "binary"}),
+    ("torchmetrics/AveragePrecision", {"task": "binary"}),
+    ("torchmetrics/JaccardIndex", {"task": "binary"}),
+    ("AccuracyManual", {}),
+    ("IoUManual", {"n_class": 2}),
+    ("NDCGManual", {}),
+    ("MRRManual", {}),
+    ("SentiMRRManual", {}),
+)
+
+_CFG_METRICS = [
+    {
+        "main": {
+            "_target_": "torchmetrics/Accuracy",
+            "task": "multiclass",
+            "num_classes": 10,
+            "top_k": 1
+        },
+        "valid_best": {"_target_": "torchmetrics/MaxMetric"},
+        "additional": {
+            "names": ["torchmetrics/AUROC"],
+            "torchmetrics/AUROC": {"task": "multiclass", "num_classes": 10}
+        }
+    },
+    {
+        "main": {"_target_": "torchmetrics/Accuracy", "task": "binary"},
+        "valid_best": {"_target_": "torchmetrics/MaxMetric"},
+        "additional": {
+            "names": ["torchmetrics/JaccardIndex", "torchmetrics/AUROC"],
+            "torchmetrics/JaccardIndex": {"task": "binary"},
+            "torchmetrics/AUROC": {"task": "binary"}
+        }
+    },
+    {
+        "main": {"_target_": "AccuracyManual"},
+        "valid_best": {"_target_": "torchmetrics/MaxMetric"}
+    }
+]
+
+
+@pytest.mark.parametrize("metric", _IMPLEMENTED_METRICS)
+def test_metric(metric: Tuple[str, Dict[str, Any]]):
+    name, params = metric
+    _ = load_metric(name, **params)
+
+
+@pytest.mark.parametrize("cfg_metric", _CFG_METRICS)
+def test_metrics(cfg_metric: Dict[str, Any]):
+    cfg = omegaconf.OmegaConf.create(cfg_metric)
+    _ = load_metrics(cfg)
