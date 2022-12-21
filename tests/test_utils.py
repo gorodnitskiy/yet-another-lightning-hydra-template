aefa20f3d431f410d3a848655c21162e1b0c05de
Alex Gorodnitskiy <alekc1272@gmail.com>
2022-12-21 12:07:19 +0300

    add utils tests

---
+++ tests/test_utils.py
@@ -0,0 +1,6 @@
+from omegaconf import DictConfig
+from src.utils import print_config_tree
+
+
+def test_rich_utils(cfg_train: DictConfig):
+    print_config_tree(cfg_train, resolve=False, save_to_file=False)
