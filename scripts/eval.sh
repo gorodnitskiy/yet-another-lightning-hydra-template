f173eb9805e58ba66efc68c657031d2b09b75f5d
Alex Gorodnitskiy <alekc1272@gmail.com>
2022-12-21 12:11:50 +0300

    add execution schedule scripts

---
+++ scripts/eval.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+# Schedule execution of many runs
+# Run from root folder with: bash scripts/eval.sh
+
+python src/eval.py
---
+++ scripts/train.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+# Schedule execution of many runs
+# Run from root folder with: bash scripts/train.sh
+
+python src/train.py
