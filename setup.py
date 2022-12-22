fd82ad9c776362c350a6296de7e5a263d69bffe1
Alex Gorodnitskiy <alekc1272@gmail.com>
2022-12-22 22:33:21 +0300

    fix setup.py style

--- setup.py
+++ setup.py
@@ -1,6 +1,5 @@
 from setuptools import find_packages, setup
 
-
 setup(
     name="src",
     version="0.1.0",
@@ -11,5 +10,5 @@ setup(
     author="Your Name",
     author_email="Your Email",
     install_requires=["pytorch-lightning", "hydra-core"],
-    packages=find_packages(exclude=['tests']),
+    packages=find_packages(exclude=["tests"]),
 )
