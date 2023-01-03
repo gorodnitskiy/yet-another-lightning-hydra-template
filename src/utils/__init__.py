ce4d6b8d113bc3ce1b2dbfc085926eae3ae514a8
Alex Gorodnitskiy <alekc1272@gmail.com>
2023-01-03 18:53:36 +0300

    move saving to saving_utils

--- src/utils/__init__.py
+++ src/utils/__init__.py
@@ -7,7 +7,7 @@ from src.utils.environment import (
 from src.utils.metadata import log_metadata
 from src.utils.pylogger import get_pylogger
 from src.utils.rich_utils import enforce_tags, print_config_tree
-from src.utils.saving import save_predictions, save_state_dicts
+from src.utils.saving_utils import save_predictions, save_state_dicts
 from src.utils.tf_utils import load_metrics, load_tf_events
 from src.utils.utils import (
     close_loggers,
