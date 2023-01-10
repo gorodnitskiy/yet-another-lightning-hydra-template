056d9f6f8277c33648f07d180c1a5b02cdd50747
Alex Gorodnitskiy <alekc1272@gmail.com>
2023-01-10 16:39:40 +0300

    refactor classification module

--- src/modules/models/classification.py
+++ src/modules/models/classification.py
@@ -1,28 +1,29 @@
 from typing import Any, List, Optional
 
 import torch
+from torch import nn
 
 from src.modules.models.module import BaseModule
 
 
-class ConvActLin(torch.nn.Module):
+class ConvActLin(nn.Module):
     def __init__(
         self,
         in_channels_2d,
         out_channels_2d,
         num_classes,
         kernel_size_2d=(1, 1),
-        activation=torch.nn.ReLU,
-        **kwargs,
+        activation=nn.ReLU,
+        **kwargs: Any,
     ) -> None:
         super().__init__()
-        self.head = torch.nn.Sequential(
-            torch.nn.Conv2d(
+        self.head = nn.Sequential(
+            nn.Conv2d(
                 in_channels_2d, out_channels_2d, kernel_size_2d, **kwargs
             ),
             activation(inplace=True),
-            torch.nn.Flatten(),
-            torch.nn.Linear(out_channels_2d, num_classes),
+            nn.Flatten(),
+            nn.Linear(out_channels_2d, num_classes),
         )
 
     def forward(self, x: torch.Tensor) -> torch.Tensor:
@@ -38,12 +39,12 @@ class Classifier(BaseModule):
         use_pretrained: bool = True,
         model_repo: Optional[str] = None,
         freeze_params: Any = None,
-        **kwargs,
+        **kwargs: Any,
     ) -> None:
         super().__init__(model_name, model_repo, use_pretrained, freeze_params)
         bias = kwargs["bias"] if "bias" in kwargs else False
         in_features = self.model.fc.in_features
-        self.model.fc = torch.nn.Linear(in_features, num_classes, bias=bias)
+        self.model.fc = nn.Linear(in_features, num_classes, bias=bias)
 
     def forward(self, x: torch.Tensor) -> torch.Tensor:
         x = self.model(x)
@@ -58,16 +59,17 @@ class ClassifierMultipleHead(BaseModule):
         use_pretrained: bool = True,
         model_repo: Optional[str] = None,
         freeze_params: Any = None,
-        **kwargs,
+        **kwargs: Any,
     ) -> None:
         super().__init__(model_name, model_repo, use_pretrained, freeze_params)
         in_features = self.model.fc.in_features
-        self.model.fc = torch.nn.Identity()
-        self.heads = []
+        self.model.fc = nn.Identity()
+        heads = []
         for num_class in num_classes:
             self.heads.append(
                 ConvActLin(in_features, in_features, num_class, **kwargs)
             )
+        self.heads = nn.ModuleList(heads)
 
     def forward(self, x: torch.Tensor) -> List[torch.Tensor]:
         x = self.model(x)
